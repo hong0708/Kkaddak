55c11fefc099625953547ee881f951db877d8eb0
HongMinKi <gaogai2@naver.com>
2023-03-08 16:58:20 +0900

    [FEAT] 스플래쉬 레이아웃 구현 (#S08P22D208-100)

---
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/join/CompleteJoinFragment.kt
@@ -0,0 +1,7 @@
+package com.ssafy.kkaddak.presentation.join
+
+import com.ssafy.kkaddak.R
+import com.ssafy.kkaddak.presentation.base.BaseFragment
+
+class CompleteJoinFragment: BaseFragment<FragmentJoinBinding>(R.layout.fragment_join) {
+}
