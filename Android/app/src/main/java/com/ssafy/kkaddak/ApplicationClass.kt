db77c23c3427e2cdc0f8ee598b11d9e413848af7
hong0708 <gaogai2@naver.com>
2023-03-11 18:55:51 +0900

    [FEAT] 카카오 로그인 페이지 추가 및 앱키 추가

--- Android/app/src/main/java/com/ssafy/kkaddak/ApplicationClass.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/ApplicationClass.kt
@@ -1,6 +1,8 @@
 package com.ssafy.kkaddak
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.kakao.sdk.common.KakaoSdk
 import com.ssafy.kkaddak.data.local.datasource.SharedPreferences
 import dagger.hilt.android.HiltAndroidApp
 
@@ -10,6 +12,8 @@ class ApplicationClass: Application() {
     override fun onCreate() {
         super.onCreate()
         preferences = SharedPreferences(applicationContext)
+        KakaoSdk.init(this, getString(R.string.KAKAO_NATIVE_APP_KEY))
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 
     companion object {
