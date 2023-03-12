808f28ae36808865a4bd7e61cbd7f1311e2d63b3
dnflwhalswl <dnflwhalswl@naver.com>
2023-03-12 21:43:02 +0900

    [FEAT] 마켓 기본페이지 작성 (#S08P22D208-42)

---
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/NftItem.kt
@@ -0,0 +1,10 @@
+package com.ssafy.kkaddak.presentation.market
+
+class NftItem(
+    var nftitem: Int,
+    var likecount: Int,
+    var creator: String,
+    var songtitle: String,
+    var date: String,
+    var price: Double
+)
