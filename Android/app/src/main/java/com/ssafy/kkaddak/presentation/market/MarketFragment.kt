91a285e9a3e00b70fab605f23644aff5cfcc560f
dnflwhalswl <dnflwhalswl@naver.com>
2023-03-13 10:32:40 +0900

    [CHORE] Merge conflict 해결

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/MarketFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/MarketFragment.kt
@@ -1,6 +1,6 @@
 package com.ssafy.kkaddak.presentation.market
 
-import androidx.recyclerview.widget.RecyclerView
+
 import com.ssafy.kkaddak.R
 import com.ssafy.kkaddak.databinding.FragmentMarketBinding
 import com.ssafy.kkaddak.presentation.base.BaseFragment
@@ -10,21 +10,22 @@ import dagger.hilt.android.AndroidEntryPoint
 class MarketFragment : BaseFragment<FragmentMarketBinding>(R.layout.fragment_market) {
 
     var nftadapter: NftItemAdapter? = null
+
     override fun initView() {
         nftinit()
         getNft()
     }
 
     private fun nftinit() {
-        val recyclerView = view?.findViewById<RecyclerView>(R.id.rv_market_nft_list)
-
-        nftadapter = NftItemAdapter()
-        recyclerView?.adapter = nftadapter
-        recyclerView?.run {
-            val spanCount = 2
-            val space = 20
-            addItemDecoration(GridSpaceItemDecoration(spanCount, space))
-        }
+//        val recyclerView = view?.findViewById<RecyclerView>(R.id.rv_market_nft_list)
+//
+//        nftadapter = NftItemAdapter()
+//        recyclerView?.adapter = nftadapter
+//        recyclerView?.run {
+//            val spanCount = 2
+//            val space = 20
+//            addItemDecoration(GridSpaceItemDecoration(spanCount, space))
+//        }
     }
     private fun getNft() {
         var data = NftItem(R.drawable.ic_nft_mockup1, 1, "창작자1", "노래 제목1", "23.03.10", 1.23)
