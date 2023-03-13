1c32e6cfd420f7033aa77bd9637d7b216287b6c3
HongMinKi <gaogai2@naver.com>
2023-03-13 17:01:25 +0900

    [FEAT] di 유저관련 사항 추가

--- Android/app/src/main/java/com/ssafy/kkaddak/di/ServiceModule.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/di/ServiceModule.kt
@@ -1,10 +1,40 @@
 package com.ssafy.kkaddak.di
 
+import com.ssafy.kkaddak.AuthInterceptorClient
+import com.ssafy.kkaddak.NoAuthInterceptorClient
+import com.ssafy.kkaddak.RefreshInterceptorClient
+import com.ssafy.kkaddak.data.remote.service.AuthApiService
+import com.ssafy.kkaddak.data.remote.service.RefreshApiService
+import com.ssafy.kkaddak.data.remote.service.UserApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthApiService(
+        @NoAuthInterceptorClient retrofit: Retrofit
+    ): AuthApiService =
+        retrofit.create(AuthApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRefreshApiService(
+        @RefreshInterceptorClient retrofit: Retrofit
+    ): RefreshApiService =
+        retrofit.create(RefreshApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideUserApiService(
+        @AuthInterceptorClient retrofit: Retrofit
+    ): UserApiService =
+        retrofit.create(UserApiService::class.java)
 }
