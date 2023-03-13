8780ab071e2cf8df7e9ff93ba7406409575c03ff
HongMinKi <gaogai2@naver.com>
2023-03-13 17:01:25 +0900

    [FEAT] di 유저관련 사항 추가

--- Android/app/src/main/java/com/ssafy/kkaddak/di/DataSourceModule.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/di/DataSourceModule.kt
@@ -1,10 +1,30 @@
 package com.ssafy.kkaddak.di
 
+import com.ssafy.kkaddak.data.remote.datasource.auth.AuthRemoteDataSourceImpl
+import com.ssafy.kkaddak.data.remote.datasource.user.UserRemoteDataSourceImpl
+import com.ssafy.kkaddak.data.remote.service.AuthApiService
+import com.ssafy.kkaddak.data.remote.service.RefreshApiService
+import com.ssafy.kkaddak.data.remote.service.UserApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataSourceModule {
+    @Provides
+    @Singleton
+    fun provideAuthDataSource(
+        authApiService: AuthApiService,
+        refreshApiService: RefreshApiService
+    ): AuthRemoteDataSourceImpl = AuthRemoteDataSourceImpl(authApiService, refreshApiService)
+
+    @Provides
+    @Singleton
+    fun provideUserDataSource(
+        userApiService: UserApiService
+    ): UserRemoteDataSourceImpl = UserRemoteDataSourceImpl(userApiService)
+
 }
--- Android/app/src/main/java/com/ssafy/kkaddak/di/UseCaseModule.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/di/UseCaseModule.kt
@@ -1,10 +1,38 @@
 package com.ssafy.kkaddak.di
 
+import com.ssafy.kkaddak.domain.repository.AuthRepository
+import com.ssafy.kkaddak.domain.repository.UserRepository
+import com.ssafy.kkaddak.domain.usecase.auth.GetNewTokenUseCase
+import com.ssafy.kkaddak.domain.usecase.auth.LoginUseCase
+import com.ssafy.kkaddak.domain.usecase.user.CheckDuplicationUseCase
+import com.ssafy.kkaddak.domain.usecase.user.CreateUserInfoUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object UseCaseModule {
+    @Singleton
+    @Provides
+    fun provideLoginUseCase(authRepository: AuthRepository): LoginUseCase =
+        LoginUseCase(authRepository)
+
+    @Singleton
+    @Provides
+    fun provideGetNewTokenUseCase(authRepository: AuthRepository): GetNewTokenUseCase =
+        GetNewTokenUseCase(authRepository)
+
+    @Singleton
+    @Provides
+    fun provideCreateUserInfoUseCase(userRepository: UserRepository): CreateUserInfoUseCase =
+        CreateUserInfoUseCase(userRepository)
+
+    @Singleton
+    @Provides
+    fun provideCheckDuplicationUseCase(userRepository: UserRepository): CheckDuplicationUseCase =
+        CheckDuplicationUseCase(userRepository)
+
 }
