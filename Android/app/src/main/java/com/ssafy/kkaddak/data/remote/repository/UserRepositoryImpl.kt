3c3b005c195230d20186da528043e0463c964f57
HongMinKi <gaogai2@naver.com>
2023-03-13 16:52:11 +0900

    [FEAT] remote repository 유저, Auth 추가

---
+++ Android/app/src/main/java/com/ssafy/kkaddak/data/remote/repository/UserRepositoryImpl.kt
@@ -0,0 +1,46 @@
+package com.ssafy.kkaddak.data.remote.repository
+
+import com.ssafy.kkaddak.common.util.wrapToResource
+import com.ssafy.kkaddak.data.remote.Resource
+import com.ssafy.kkaddak.data.remote.datasource.user.UserRemoteDataSource
+import com.ssafy.kkaddak.domain.entity.user.User
+import kotlinx.coroutines.Dispatchers
+import okhttp3.MultipartBody
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val userRemoteDataSource: UserRemoteDataSource
+) : UserRepository {
+
+    override suspend fun createUserInfo(
+        nickName: String,
+        profileImg: MultipartBody.Part?,
+        fcmToken: String
+    ): Resource<User> =
+        wrapToResource(Dispatchers.IO) {
+            userRemoteDataSource.createUserInfo(nickName, profileImg, fcmToken).toDomainModel()
+        }
+
+    override suspend fun createUserInfoWithoutImg(
+        nickName: String,
+        fcmToken: String
+    ): Resource<User> =
+        wrapToResource(Dispatchers.IO) {
+            userRemoteDataSource.createUserInfoWithoutImg(nickName, fcmToken).toDomainModel()
+        }
+
+    override suspend fun checkDuplication(nickName: String): Resource<Boolean> =
+        wrapToResource(Dispatchers.IO) {
+            userRemoteDataSource.checkDuplication(nickName)
+        }
+
+    override suspend fun cancelSignUp(): Resource<Boolean> =
+        wrapToResource(Dispatchers.IO) {
+            userRemoteDataSource.cancelSignUp()
+        }
+
+    override suspend fun getUserProfile(): Resource<UserProfile> =
+        wrapToResource(Dispatchers.IO){
+            userRemoteDataSource.getUserProfileImg().toDomainModel()
+        }
+}
