ae2b4e63ca2a0b8ac665cbe0b524f0788e9400af
dnflwhalswl <dnflwhalswl@naver.com>
2023-03-13 14:09:34 +0900

    [FEAT] 경매 등록 페이지 구현 (#S08P22D208-43, #S08P22D208-50, #S08P22D208-51)

---
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/UploadMarketFragment.kt
@@ -0,0 +1,12 @@
+package com.ssafy.kkaddak.presentation.market
+
+import com.ssafy.kkaddak.R
+import com.ssafy.kkaddak.databinding.FragmentCompleteJoinBinding
+import com.ssafy.kkaddak.presentation.base.BaseFragment
+
+class UploadMarketFragment :
+    BaseFragment<FragmentCompleteJoinBinding>(R.layout.fragment_upload_market) {
+    override fun initView() {
+
+    }
+}
