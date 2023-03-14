0a1baaf991e7ec22f3b1ae43b3f5425eeeacfaf1
HongMinKi <gaogai2@naver.com>
2023-03-14 10:30:03 +0900

    [FEAT] 로그인 레이아웃 수정 및 로그인 플로우 연동

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/join/JoinFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/join/JoinFragment.kt
@@ -3,9 +3,10 @@ package com.ssafy.kkaddak.presentation.join
 import com.ssafy.kkaddak.R
 import com.ssafy.kkaddak.databinding.FragmentJoinBinding
 import com.ssafy.kkaddak.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class JoinFragment : BaseFragment<FragmentJoinBinding>(R.layout.fragment_join) {
-    override fun initView() {
 
-    }
+    override fun initView() {}
 }
--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/join/LoginFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/join/LoginFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.viewModels
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
 import com.ssafy.kkaddak.R
+import com.ssafy.kkaddak.common.util.fadeInView
 import com.ssafy.kkaddak.data.remote.datasource.auth.AuthRequest
 import com.ssafy.kkaddak.databinding.FragmentLoginBinding
 import com.ssafy.kkaddak.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
@@ -16,7 +21,18 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
     private val joinViewModel by viewModels<JoinViewModel>()
     private val callback = initKakaoLoginCallback()
 
-    override fun initView() {}
+    override fun initView() {
+        initListener()
+        observeJoinViewModel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        CoroutineScope(Dispatchers.Main).launch {
+            fadeInView(binding.clLogin, requireContext())
+            delay(1000)
+        }
+    }
 
     private fun initListener() {
         binding.clKakaoLogin.setOnClickListener {
@@ -31,6 +47,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
     private fun initKakaoLoginCallback(): (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error != null) {
             Log.e("login", "카카오계정으로 로그인 실패", error)
+            navigate(LoginFragmentDirections.actionLoginFragmentToJoinFragment())
         } else if (token != null) {
             Log.i("login", "카카오계정으로 로그인 성공 ${token.accessToken}")
             joinViewModel.requestLogin(AuthRequest(token.accessToken))
@@ -44,10 +61,10 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
         joinViewModel.isExist.observe(viewLifecycleOwner) {
             when (it) {
                 true -> {
-                    //navigate(actionTrue)
+                    navigate(LoginFragmentDirections.actionLoginFragmentToMainActivity())
                 }
                 false -> {
-                    //navigate(actionFalse)
+                    navigate(LoginFragmentDirections.actionLoginFragmentToJoinFragment())
                 }
                 else -> {}
             }
