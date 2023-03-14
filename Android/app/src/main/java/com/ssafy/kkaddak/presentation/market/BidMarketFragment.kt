1ee37a012758a587705f037b2727a055b6967fe7
dnflwhalswl <dnflwhalswl@naver.com>
2023-03-14 10:08:07 +0900

    [FEAT] 경매 입찰 페이지 Fragment 추가

---
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/BidMarketFragment.kt
@@ -0,0 +1,12 @@
+package com.ssafy.kkaddak.presentation.market
+
+import com.ssafy.kkaddak.R
+import com.ssafy.kkaddak.databinding.FragmentCompleteJoinBinding
+import com.ssafy.kkaddak.presentation.base.BaseFragment
+
+class BidMarketFragment :
+    BaseFragment<FragmentCompleteJoinBinding>(R.layout.fragment_bid_market) {
+    override fun initView() {
+
+    }
+}
