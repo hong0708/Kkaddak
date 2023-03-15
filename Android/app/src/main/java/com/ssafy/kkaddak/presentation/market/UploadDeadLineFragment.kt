c0d327479a91dd17fb9e1ed0ff58fa081e9836c7
dnflwhalswl <dnflwhalswl@naver.com>
2023-03-15 14:30:41 +0900

    [CHORE] 리뷰 반영

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/UploadDeadLineFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/UploadDeadLineFragment.kt
@@ -5,12 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.NumberPicker
-import android.widget.TextView
 import androidx.fragment.app.DialogFragment
-import com.ssafy.kkaddak.R
+import com.ssafy.kkaddak.databinding.DialogUploadNftDeadlineBinding
 
 class UploadDeadLineFragment : DialogFragment() {
 
+    private var _binding: DialogUploadNftDeadlineBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //false로 설정해 주면 화면밖 혹은 뒤로가기 버튼시 다이얼로그라 dismiss 되지 않는다.
@@ -21,8 +22,8 @@ class UploadDeadLineFragment : DialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.dialog_upload_nft_deadline, container, false)
+        _binding = DialogUploadNftDeadlineBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     private fun setDate(np: NumberPicker?, values: Array<String>) {
@@ -38,26 +39,18 @@ class UploadDeadLineFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val btnOk = view?.findViewById<TextView>(R.id.tv_confirm_nft_item)
-        val btnCancel = view?.findViewById<TextView>(R.id.tv_cancel_nft_item)
-        val npYear = view?.findViewById<NumberPicker>(R.id.np_year)
-        val npMonth = view?.findViewById<NumberPicker>(R.id.np_month)
-        val npDay = view?.findViewById<NumberPicker>(R.id.np_day)
         val years = arrayOf("2023년", "2024년", "2025년", "2026년", "2027년", "2028년", "2029년", "2030년")
-        val months =
-            arrayOf("1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월")
+        val months = arrayOf("1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월")
         val days = Array(31, {""})
-
         for (day in 0..30) {
             days.set(day, (day + 1).toString() + "일")
         }
 
-        setDate(npYear, years)
-        setDate(npMonth, months)
-        setDate(npDay, days)
+        setDate(binding.npYear, years)
+        setDate(binding.npMonth, months)
+        setDate(binding.npDay, days)
 
-        btnOk?.setOnClickListener {  }
-        btnCancel?.setOnClickListener {  }
+        binding.tvConfirmNftItem?.setOnClickListener {  }
+        binding.tvCancelNftItem?.setOnClickListener {  }
     }
-
 }
--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/UploadMarketFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/UploadMarketFragment.kt
@@ -1,22 +1,14 @@
 package com.ssafy.kkaddak.presentation.market
 
-import android.widget.TextView
 import com.ssafy.kkaddak.R
 import com.ssafy.kkaddak.databinding.FragmentUploadMarketBinding
 import com.ssafy.kkaddak.presentation.base.BaseFragment
 
 class UploadMarketFragment :
     BaseFragment<FragmentUploadMarketBinding>(R.layout.fragment_upload_market) {
+
     override fun initView() {
-        val tvUploadNft = view?.findViewById<TextView>(R.id.tv_select_nft_image)
-        val tvUploadDeadline = view?.findViewById<TextView>(R.id.tv_select_deadline)
-//        val tvDeadline = view?.findViewById<TextView>(R.id.tv_deadline)
-        tvUploadNft?.setOnClickListener {
-            // Show Dialog(dialog_upload_nft_detail)
-        }
-        tvUploadDeadline?.setOnClickListener {
-            // Show Dialog(dialog_upload_nft_deadline)
-            // tvDeadline.setText(마감날짜)
-        }
+        binding.tvSelectNftImage.setOnClickListener{  }
+        binding.tvUploadDeadline.setOnClickListener {  }
     }
 }
