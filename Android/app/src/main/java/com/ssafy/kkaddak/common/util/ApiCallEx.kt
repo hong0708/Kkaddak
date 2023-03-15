a9835ac722d20b1dbcbc99b6c4b6418f7402592f
HongMinKi <gaogai2@naver.com>
2023-03-15 10:56:22 +0900

    [CHORE] Safe API Call에 따른 코루틴 수정

--- Android/app/src/main/java/com/ssafy/kkaddak/common/util/ApiCallEx.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/common/util/ApiCallEx.kt
@@ -1,22 +1,25 @@
 package com.ssafy.kkaddak.common.util
 
 import com.ssafy.kkaddak.data.remote.Resource
-import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
 
-suspend fun <T> wrapToResource(dispatcher: CoroutineDispatcher, apiCall: suspend () -> T): Resource<T> {
-    return try {
-        Resource.Success(apiCall())
-    } catch (throwable: Throwable) {
-        when(throwable) {
-            is IOException -> Resource.Error(throwable.message ?: "ERROR")
-            is HttpException -> {
-                val code = throwable.code()
-                Resource.Error(code.toString())
-            }
-            else -> {
-                Resource.Error(throwable.message ?: "ERROR")
+suspend fun <T> wrapToResource(apiCall: suspend () -> T): Resource<T> {
+    return withContext(Dispatchers.IO) {
+        try {
+            Resource.Success(apiCall())
+        } catch (throwable: Throwable) {
+            when(throwable) {
+                is IOException -> Resource.Error(throwable.message ?: "ERROR")
+                is HttpException -> {
+                    val code = throwable.code()
+                    Resource.Error(code.toString())
+                }
+                else -> {
+                    Resource.Error(throwable.message ?: "ERROR")
+                }
             }
         }
     }
