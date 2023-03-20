208e00086eac66197583d5cd62816565044bf78e
HongMinKi <gaogai2@naver.com>
2023-03-20 16:15:59 +0900

    [FEAT] 안드로이드 키스토어 사용 시 보안 설정 추가

--- Android/app/src/main/java/com/ssafy/kkaddak/ApplicationClass.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/ApplicationClass.kt
@@ -5,15 +5,38 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.ssafy.kkaddak.data.local.datasource.SharedPreferences
 import dagger.hilt.android.HiltAndroidApp
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.security.Provider
+import java.security.Security
 
 @HiltAndroidApp
-class ApplicationClass: Application() {
+class ApplicationClass : Application() {
 
     override fun onCreate() {
         super.onCreate()
         preferences = SharedPreferences(applicationContext)
         KakaoSdk.init(this, getString(R.string.KAKAO_NATIVE_APP_KEY))
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        setupBouncyCastle()
+    }
+
+    private fun setupBouncyCastle() {
+        val provider: Provider? = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
+        if (provider == null) {
+            // Web3j will set up the provider lazily when it's first used.
+            return
+        }
+        if (provider.javaClass == BouncyCastleProvider::class.java) {
+            // BC with same package name, shouldn't happen in real life.
+            return
+        }
+        // Android registers its own BC provider. As it might be outdated and might not include
+        // all needed ciphers, we substitute it with a known BC bundled in the app.
+        // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
+        // of that it's possible to have another BC implementation loaded in VM.
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
+        Security.insertProviderAt(BouncyCastleProvider(), 1)
     }
 
     companion object {
