bc114b5766106b437475177691ce6b501acbdb47
HongMinKi <gaogai2@naver.com>
2023-03-20 17:56:19 +0900

    [CHORE] 안드로이드 web3j setupBouncyCastle 수정

--- Android/app/src/main/java/com/ssafy/kkaddak/ApplicationClass.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/ApplicationClass.kt
@@ -21,7 +21,26 @@ class ApplicationClass : Application() {
         setupBouncyCastle()
     }
 
+
+    /*if (provider == null) 문장 삭제
+    null 체크를 하지 않아도 provider?.javaClass 로 안전하게 체크할 수 있습니다.
+    if (provider.javaClass == BouncyCastleProvider::class.java) 문장 변경
+    Kotlin의 null safe operator ?. 와 != 연산자를 사용하여 더 간결하게 표현하였습니다.
+    주석 추가 및 변경
+    주석을 더 명확하게 작성하고, 필요없는 주석은 삭제하였습니다.
+    BouncyCastleProvider를 등록하는 부분 변경
+    provider가 null이 아니면서 BouncyCastleProvider가 아니면 Security에 BouncyCastleProvider를 등록합니다.*/
+
     private fun setupBouncyCastle() {
+        val provider: Provider? = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
+        if (provider?.javaClass != BouncyCastleProvider::class.java) {
+            // Register BouncyCastleProvider if it's not already registered
+            Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
+            Security.insertProviderAt(BouncyCastleProvider(), 1)
+        }
+    }
+
+    private fun setupBouncyCastle1() {
         val provider: Provider? = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
         if (provider == null) {
             // Web3j will set up the provider lazily when it's first used.
