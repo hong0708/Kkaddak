ac294498bb2ea04221a241965580318ecafaca08
Wonil Lee <lwi99556@gmail.com>
2023-03-21 03:45:09 +0900

    [FIX]Exception 처리 및 status code 수정

--- Server/module-core/src/main/java/com/example/kkaddak/core/utils/ErrorMessageEnum.java
+++ Server/module-core/src/main/java/com/example/kkaddak/core/utils/ErrorMessageEnum.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorMessageEnum {
-    USER_NOT_EXIST("존재하지 않는 회원입니다");
+    USER_NOT_EXIST("존재하지 않는 회원입니다"),
+    FOLLOW_NOT_EXIST("팔로우하고 있지 않는 유저입니다.");
 
     private String message;
 
