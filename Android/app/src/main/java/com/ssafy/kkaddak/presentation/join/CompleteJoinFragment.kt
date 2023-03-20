668f1fbdd62d4431c39967e7eec8f8273ba48f5d
HongMinKi <gaogai2@naver.com>
2023-03-20 15:18:30 +0900

    [FEAT] kakao join 기능 구현 및 닉네임 확인 오류 수정

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/join/CompleteJoinFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/join/CompleteJoinFragment.kt
@@ -1,12 +1,17 @@
 package com.ssafy.kkaddak.presentation.join
 
+import android.content.Intent
+import androidx.lifecycle.lifecycleScope
 import com.ssafy.kkaddak.R
 import com.ssafy.kkaddak.common.util.fadeOutView
 import com.ssafy.kkaddak.databinding.FragmentCompleteJoinBinding
+import com.ssafy.kkaddak.presentation.MainActivity
 import com.ssafy.kkaddak.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -17,6 +22,11 @@ class CompleteJoinFragment :
         CoroutineScope(Dispatchers.Main).launch {
             delay(1000)
             fadeOutView(binding.clWelcomeKkaddak, requireContext())
+            delay(1000)
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
             delay(1000)
             navigate(CompleteJoinFragmentDirections.actionCompleteJoinFragmentToMainActivity())
         }
