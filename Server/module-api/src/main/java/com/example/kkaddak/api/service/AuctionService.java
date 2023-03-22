6efb7b5ad46c4019afdbbb0e2ea5f006e2e88568
Wonil Lee <lwi99556@gmail.com>
2023-03-23 03:41:57 +0900

    [FEAT] 경매 목록 조회 api 개발(#S08P22D208-259)

--- Server/module-api/src/main/java/com/example/kkaddak/api/service/AuctionService.java
+++ Server/module-api/src/main/java/com/example/kkaddak/api/service/AuctionService.java
@@ -1,5 +1,7 @@
 package com.example.kkaddak.api.service;
 
+import com.example.kkaddak.api.exception.NoContentException;
+import com.example.kkaddak.core.dto.AuctionConditionReqDto;
 import com.example.kkaddak.core.dto.AuctionReqDto;
 import com.example.kkaddak.api.dto.DataResDto;
 import com.example.kkaddak.core.entity.Member;
@@ -9,5 +11,5 @@ import org.springframework.stereotype.Service;
 public interface AuctionService {
     DataResDto<?> createAuction(AuctionReqDto auctionReqDto, Member member);
 
-    DataResDto<?> getAuctionAllByPaging(Long limit, Long lastId) throws Exception;
+    DataResDto<?> getAuctionAllByCondition(AuctionConditionReqDto condition, Member member) throws NoContentException;
 }
