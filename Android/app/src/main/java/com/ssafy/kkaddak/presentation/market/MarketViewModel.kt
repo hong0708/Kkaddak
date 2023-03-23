c16341bcf87b675d9b900a92e5b8aeef98a82400
조민지 <dnflwhalswl@naver.com>
2023-03-24 07:05:57 +0900

    [FEAT] NFT 목록 무한스크롤 구현 (#S08P22D208-281)

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/MarketViewModel.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/MarketViewModel.kt
@@ -1,10 +1,7 @@
 package com.ssafy.kkaddak.presentation.market
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.ssafy.kkaddak.data.remote.Resource
 import com.ssafy.kkaddak.domain.entity.market.NftItem
 import com.ssafy.kkaddak.domain.usecase.market.GetAllNftsUseCase
@@ -20,15 +17,59 @@ class MarketViewModel @Inject constructor(
     private val _nftListData: MutableLiveData<List<NftItem>?> = MutableLiveData()
     val nftListData: LiveData<List<NftItem>?> = _nftListData
 
+    private val _nftTempData: MutableLiveData<List<NftItem>?> = MutableLiveData()
+    val nftTempData: LiveData<List<NftItem>?> = _nftTempData
+
     fun getAllNfts(lastId: Long, limit: Long, onlySelling: Boolean) = viewModelScope.launch {
         when (val value = getAllNftsUseCase(lastId, limit, onlySelling)) {
             is Resource.Success<List<NftItem>> -> {
-                _nftListData.value = value.data
+                _nftTempData.value = value.data
             }
             is Resource.Error -> {
                 Log.e("getAllNfts", "getAllNfts: ${value.errorMessage}")
             }
         }
+    }
+
+    // 기존 리스트의 마지막 아이디보다 새로 불러온 리스트의 첫 아이디가 큰 경우는 중복으로 판단
+    fun dup(list1: List<NftItem>, list2: List<NftItem>) : Boolean {
+        if(list1.get(list1.size - 1).acutionId <= list2.get(list2.size - 1).acutionId) {
+            return true
+        }
+        return false
+    }
+
+    fun getSum() {
+        val list1: List<NftItem>? = _nftListData.value
+        val list2: List<NftItem>? = _nftTempData.value
+
+        val joinedList: MutableList<NftItem> = ArrayList()
+
+        // 기존 리스트와 새로 불러온 리스트 합치기
+        list1?.let { joinedList.addAll(it) }
+        list2?.let { joinedList.addAll(it) }
+
+        // 중복 부분 제거
+        if(list1 != null && list2 != null) {
+            if (dup(list1, list2)) {
+                for (i in 0..19) {
+                    joinedList.removeAt(joinedList.size - 1)
+                }
+            }
+        }
+        _nftListData.value = joinedList
+    }
+
+    fun getLastId(): Long? {
+        return _nftListData.value?.size?.let { _nftListData.value?.get(it - 1) }?.acutionId?.toLong()
+    }
+
+    fun getTempSize(): Int? {
+        return _nftTempData.value?.size
+    }
+
+    fun getSize(): Int? {
+        return _nftListData.value?.size
     }
 
 }
