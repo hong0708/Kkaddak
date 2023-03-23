dc3197660e60eee74f3697b64adfbfcac8580fbd
Wonil Lee <lwi99556@gmail.com>
2023-03-23 13:46:35 +0900

    [FEAT] 경매 북마크 기능 추가(#S08P22D208-320)

--- Server/module-api/src/main/java/com/example/kkaddak/api/service/impl/AuctionServiceImpl.java
+++ Server/module-api/src/main/java/com/example/kkaddak/api/service/impl/AuctionServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.kkaddak.api.service.impl;
 
 
+import com.example.kkaddak.api.dto.member.FollowResDto;
 import com.example.kkaddak.api.exception.NoContentException;
+import com.example.kkaddak.api.exception.NotFoundException;
 import com.example.kkaddak.api.exception.PagingQueryException;
 import com.example.kkaddak.core.dto.AuctionConditionReqDto;
 import com.example.kkaddak.core.dto.AuctionConditionResDto;
@@ -10,8 +12,12 @@ import com.example.kkaddak.api.dto.auction.AuctionResDto;
 import com.example.kkaddak.api.dto.DataResDto;
 import com.example.kkaddak.api.service.AuctionService;
 import com.example.kkaddak.core.entity.Auction;
+import com.example.kkaddak.core.entity.Follow;
+import com.example.kkaddak.core.entity.LikeAuction;
 import com.example.kkaddak.core.entity.Member;
 import com.example.kkaddak.core.repository.AuctionRepository;
+import com.example.kkaddak.core.repository.LikeAuctionRepository;
+import com.example.kkaddak.core.utils.ErrorMessageEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -26,7 +32,7 @@ import java.util.stream.Collectors;
 public class AuctionServiceImpl implements AuctionService {
 
     private final AuctionRepository auctionRepo;
-    private final EntityManager entityManager;
+    private final LikeAuctionRepository likeAuctionRepository;
 
     @Override
     public DataResDto<?> createAuction(AuctionReqDto auctionReqDto, Member seller) {
@@ -46,5 +52,25 @@ public class AuctionServiceImpl implements AuctionService {
                 .statusMessage("조건에 따라 경매 목록이 조회되었습니다.")
                 .data(res)
                 .build();
+    }
+
+    @Override
+    public DataResDto<?> likeAuction(Member liker, int auctionId) {
+        Auction auction = auctionRepo.findById(auctionId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessageEnum.AUCTION_NOT_EXIST.getMessage()));
+        LikeAuction likeAuction = LikeAuction.builder().liker(liker).auction(auction).build();
+        likeAuctionRepository.save(likeAuction);
+        return DataResDto.builder().statusMessage("해당 경매가 북마크되었습니다.").data(true).build();
+    }
+
+    @Override
+    public DataResDto<?> unlikeAuction(Member liker, int auctionId) {
+        Auction auction = auctionRepo.findById(auctionId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessageEnum.AUCTION_NOT_EXIST.getMessage()));
+        LikeAuction likeAuction = likeAuctionRepository.findByLikerAndAuction(liker, auction)
+                .orElseThrow(() -> new NotFoundException(ErrorMessageEnum.LIKEAUCTION_NOT_EXIST.getMessage()));
+
+        likeAuctionRepository.delete(likeAuction);
+        return DataResDto.builder().statusCode(204).statusMessage("해당 경매의 북마크가 취소되었습니다.").data(true).build();
     }
 }
