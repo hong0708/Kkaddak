15313b235430e11775a9ec9af465dbe4f01d7313
HongMinKi <gaogai2@naver.com>
2023-03-24 09:46:37 +0900

    [FEAT] web3j설정 및 지갑 생성 (#S08P22D208-211)

--- Android/app/src/main/java/com/ssafy/kkaddak/ApplicationClass.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/ApplicationClass.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.ssafy.kkaddak.data.local.datasource.SharedPreferences
 import dagger.hilt.android.HiltAndroidApp
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.security.Provider
+import java.security.Security
 
 @HiltAndroidApp
 class ApplicationClass: Application() {
@@ -14,6 +17,17 @@ class ApplicationClass: Application() {
         preferences = SharedPreferences(applicationContext)
         KakaoSdk.init(this, getString(R.string.KAKAO_NATIVE_APP_KEY))
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        setupBouncyCastle()
+    }
+
+    private fun setupBouncyCastle() {
+        val provider: Provider? = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
+        if (provider?.javaClass != BouncyCastleProvider::class.java) {
+            // Register BouncyCastleProvider if it's not already registered
+            Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
+            Security.insertProviderAt(BouncyCastleProvider(), 1)
+        }
     }
 
     companion object {
