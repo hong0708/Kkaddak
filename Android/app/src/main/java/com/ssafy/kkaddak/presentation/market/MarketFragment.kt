09851348404f8016057112d5e3aa8169cc587d27
조민지 <dnflwhalswl@naver.com>
2023-03-24 09:57:37 +0900

    [FEAT] NFT 목록 Selling 필터링 구현 (#S08P22D208-181)

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/MarketFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/MarketFragment.kt
@@ -26,16 +26,13 @@ class MarketFragment : BaseFragment<FragmentMarketBinding>(R.layout.fragment_mar
                 .commit()
         }
         binding.tvAllNfts.setOnClickListener { onButtonClick(binding.tvAllNfts) }
-        binding.tvSellingNft.setOnClickListener {
-            onButtonClick(binding.tvSellingNft)
-
-        }
+        binding.tvSellingNft.setOnClickListener { onButtonClick(binding.tvSellingNft) }
         binding.tvBiddingNft.setOnClickListener { onButtonClick(binding.tvBiddingNft) }
         binding.tvBookmarkNft.setOnClickListener { onButtonClick(binding.tvBookmarkNft) }
     }
 
     private fun initRecyclerView() {
-
+        marketViewModel.clearData()
         marketViewModel.getAllNfts(-1, 20, false)
 
         nftadapter = NftItemAdapter()
@@ -46,6 +43,36 @@ class MarketFragment : BaseFragment<FragmentMarketBinding>(R.layout.fragment_mar
             val spanCount = 2
             val space = 20
             addItemDecoration(GridSpaceItemDecoration(spanCount, space))
+        }
+
+        marketViewModel.nftTempData.observe(viewLifecycleOwner) { marketViewModel.getSum() }
+
+        marketViewModel.nftListData.observe(viewLifecycleOwner) { response -> response?.let { nftadapter!!.setNfts(it) } }
+
+        binding.rvMarketNftList.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val lastVisibleItemPosition = (recyclerView.layoutManager as GridLayoutManager?)!!.findLastCompletelyVisibleItemPosition() // 화면에 보이는 마지막 아이템의 position
+                val itemTotalCount = recyclerView.adapter!!.itemCount - 1 // 어댑터에 등록된 아이템의 총 개수 -1
+
+                // 스크롤이 최하단에 도착했을 때
+                if(!isLoading && !binding.rvMarketNftList.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount) {
+                    isLoading = true
+                    addData(false)
+                }
+            }
+        })
+    }
+    private fun allRecyclerView() {
+        marketViewModel.clearData()
+        marketViewModel.getAllNfts(-1, 20, false)
+
+        nftadapter = NftItemAdapter()
+        binding.rvMarketNftList.apply {
+            adapter = nftadapter
+            layoutManager =
+                GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
         }
 
         marketViewModel.nftTempData.observe(viewLifecycleOwner) {
@@ -66,18 +93,53 @@ class MarketFragment : BaseFragment<FragmentMarketBinding>(R.layout.fragment_mar
                 // 스크롤이 최하단에 도착했을 때
                 if(!isLoading && !binding.rvMarketNftList.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount) {
                     isLoading = true
-                    addData()
+                    addData(false)
                 }
             }
         })
     }
 
-    private fun addData() {
+    private fun sellingRecyclerView() {
+        marketViewModel.clearData()
+        marketViewModel.getAllNfts(-1, 20, true)
+
+        nftadapter = NftItemAdapter()
+        binding.rvMarketNftList.apply {
+            adapter = nftadapter
+            layoutManager =
+                GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+        }
+
+        marketViewModel.nftTempData.observe(viewLifecycleOwner) {
+            marketViewModel.getSum()
+        }
+
+        marketViewModel.nftListData.observe(viewLifecycleOwner) { response ->
+            response?.let { nftadapter!!.setNfts(it) }
+        }
+
+        binding.rvMarketNftList.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val lastVisibleItemPosition = (recyclerView.layoutManager as GridLayoutManager?)!!.findLastCompletelyVisibleItemPosition() // 화면에 보이는 마지막 아이템의 position
+                val itemTotalCount = recyclerView.adapter!!.itemCount - 1 // 어댑터에 등록된 아이템의 총 개수 -1
+
+                // 스크롤이 최하단에 도착했을 때
+                if(!isLoading && !binding.rvMarketNftList.canScrollVertically(1) && lastVisibleItemPosition == itemTotalCount) {
+                    isLoading = true
+                    addData(true)
+                }
+            }
+        })
+    }
+
+    private fun addData(selling: Boolean) {
         if(marketViewModel.getTempSize() == null) return
         if(marketViewModel.getTempSize()!! >= 20) {
             if(marketViewModel.getSize() != null) {
                 val lastId = marketViewModel.getLastId()
-                lastId?.let { marketViewModel.getAllNfts(it, 20, false) }
+                lastId?.let { marketViewModel.getAllNfts(it, 20, selling) }
                 marketViewModel.nftTempData.observe(viewLifecycleOwner) {
                     marketViewModel.getSum()
                 }
@@ -86,7 +148,7 @@ class MarketFragment : BaseFragment<FragmentMarketBinding>(R.layout.fragment_mar
         isLoading = false
     }
 
-    fun onButtonClick(tv: TextView) {
+    private fun onButtonClick(tv: TextView) {
         when (tv) {
             binding.tvAllNfts -> {
                 tv.setBackgroundResource(R.drawable.bg_rect_bitter_sweet_to_neon_pink_bastille_radius50_stroke2)
@@ -95,6 +157,7 @@ class MarketFragment : BaseFragment<FragmentMarketBinding>(R.layout.fragment_mar
                     tvBiddingNft.setBackgroundResource(R.drawable.bg_rect_bastille_radius50)
                     tvBookmarkNft.setBackgroundResource(R.drawable.bg_rect_bastille_radius50)
                 }
+                allRecyclerView()
             }
             binding.tvSellingNft -> {
                 tv.setBackgroundResource(R.drawable.bg_rect_bitter_sweet_to_neon_pink_bastille_radius50_stroke2)
@@ -103,6 +166,7 @@ class MarketFragment : BaseFragment<FragmentMarketBinding>(R.layout.fragment_mar
                     tvBiddingNft.setBackgroundResource(R.drawable.bg_rect_bastille_radius50)
                     tvBookmarkNft.setBackgroundResource(R.drawable.bg_rect_bastille_radius50)
                 }
+                sellingRecyclerView()
             }
             binding.tvBiddingNft -> {
                 tv.setBackgroundResource(R.drawable.bg_rect_bitter_sweet_to_neon_pink_bastille_radius50_stroke2)
