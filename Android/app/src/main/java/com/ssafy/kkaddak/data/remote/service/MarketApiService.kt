d13c24f10b6e33fe4cf5d441410a9d68f8b9b7a8
조민지 <dnflwhalswl@naver.com>
2023-03-24 17:54:41 +0900

    [FEAT] 경매 입찰페이지 data binding 구현 (#S08P22D208-277)

--- Android/app/src/main/java/com/ssafy/kkaddak/data/remote/service/MarketApiService.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/data/remote/service/MarketApiService.kt
@@ -3,15 +3,24 @@ package com.ssafy.kkaddak.data.remote.service
 import com.ssafy.kkaddak.data.remote.datasource.base.BaseResponse
 import com.ssafy.kkaddak.data.remote.datasource.market.NftItemResponse
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
+
 
 interface MarketApiService {
 
     @GET("/api/v3/auction/condition")
     suspend fun getAllNfts(
-        @Query("lastId") lastId: Long,
-        @Query("limit") limit: Long,
+        @Query("lastId") lastId: Int,
+        @Query("limit") limit: Int,
         @Query("onlySelling") onlySelling: Boolean
     ) : BaseResponse<List<NftItemResponse>>
+
+    @POST("/api/v3/auction/like/{auctionId}")
+    suspend fun requestMarketBookmark(@Path("auctionId") auctionId: Int)
+
+    @POST("/api/v3/auction/unlike/{auctionId}")
+    suspend fun cancelMarketBookmark(@Path("auctionId") auctionId: Int)
 
 }
--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/UploadMarketFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/UploadMarketFragment.kt
@@ -6,7 +6,9 @@ import com.ssafy.kkaddak.R
 import com.ssafy.kkaddak.databinding.FragmentUploadMarketBinding
 import com.ssafy.kkaddak.domain.entity.market.UploadNftItem
 import com.ssafy.kkaddak.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class UploadMarketFragment :
     BaseFragment<FragmentUploadMarketBinding>(R.layout.fragment_upload_market) {
 
