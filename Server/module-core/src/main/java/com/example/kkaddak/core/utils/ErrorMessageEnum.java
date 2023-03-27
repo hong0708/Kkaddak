71a5d32e99e3e219800cd5186018b8b359668eff
Wonil Lee <lwi99556@gmail.com>
2023-03-27 15:14:37 +0900

    [FIX] countByMember 메서드 추가 및 예외처리 메세지 수정

--- Server/module-core/src/main/java/com/example/kkaddak/core/utils/ErrorMessageEnum.java
+++ Server/module-core/src/main/java/com/example/kkaddak/core/utils/ErrorMessageEnum.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorMessageEnum {
     USER_NOT_EXIST("존재하지 않는 회원입니다"),
-    MARKET_NOT_EXIST("존재하지 않는 경매 아이템입니다."),
-    LIKEMARKET_NOT_EXIST("북마크하지 않은 경매입니다."),
-    NO_MORE_MARKET("경매 목록이 존재하지 않습니다."),
+    MARKET_NOT_EXIST("존재하지 않는 마켓 아이템입니다."),
+    LIKEMARKET_NOT_EXIST("북마크하지 않은 마켓입니다."),
+    NO_MORE_MARKET("마켓 목록이 존재하지 않습니다."),
     NO_MORE_FOLLOWER("팔로워가 존재하지 않습니다."),
     NO_MORE_FOLLOWING("팔로잉이 존재하지 않습니다."),
     FOLLOW_NOT_EXIST("팔로우하고 있지 않는 유저입니다.");
