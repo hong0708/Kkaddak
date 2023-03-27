83e6db1e4423089da2a1605e21726eece99c2ffb
조민지 <dnflwhalswl@naver.com>
2023-03-27 13:48:46 +0900

    [FEAT] 판매 히스토리 추가 (#S08P22D208-339)

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/BuyMarketFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/market/BuyMarketFragment.kt
@@ -4,17 +4,17 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.ssafy.kkaddak.R
-import com.ssafy.kkaddak.databinding.FragmentBidMarketBinding
+import com.ssafy.kkaddak.databinding.FragmentBuyMarketBinding
 import com.ssafy.kkaddak.presentation.MainActivity
 import com.ssafy.kkaddak.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class BidMarketFragment :
-    BaseFragment<FragmentBidMarketBinding>(R.layout.fragment_bid_market) {
+class BuyMarketFragment :
+    BaseFragment<FragmentBuyMarketBinding>(R.layout.fragment_buy_market) {
 
-    private val args by navArgs<BidMarketFragmentArgs>()
+    private val args by navArgs<BuyMarketFragmentArgs>()
     private val marketViewModel by activityViewModels<MarketViewModel>()
 
     override fun initView() {
@@ -27,9 +27,9 @@ class BidMarketFragment :
         marketViewModel.nftData.observe(viewLifecycleOwner) {
             binding.nftItem = it
             if (it!!.isLike) {
-                binding.ivBidNftLike.setImageResource(R.drawable.ic_market_like_selected)
+                binding.ivNftLike.setImageResource(R.drawable.ic_market_like_selected)
             } else {
-                binding.ivBidNftLike.setImageResource(R.drawable.ic_market_like)
+                binding.ivNftLike.setImageResource(R.drawable.ic_market_like)
             }
         }
         marketViewModel.getData(args.nftItem)
@@ -37,16 +37,16 @@ class BidMarketFragment :
 
     private fun initListener() {
         binding.ivBack.setOnClickListener { popBackStack() }
-        binding.ivBidNftLike.setOnClickListener {
+        binding.ivNftLike.setOnClickListener {
             val isLike = args.nftItem.isLike
             if(!isLike) {
                 lifecycleScope.launch {
                     val like = marketViewModel.requestBookmark(args.nftItem.auctionId)
                     if (like == "true") {
-                        binding.ivBidNftLike.setBackgroundResource(R.drawable.ic_market_like_selected)
+                        binding.ivNftLike.setBackgroundResource(R.drawable.ic_market_like_selected)
                         args.nftItem.isLike = true
                     } else if (like == "false") {
-                        binding.ivBidNftLike.setBackgroundResource(R.drawable.ic_market_like)
+                        binding.ivNftLike.setBackgroundResource(R.drawable.ic_market_like)
                         args.nftItem.isLike = false
                     }
                 }
@@ -54,17 +54,17 @@ class BidMarketFragment :
                 lifecycleScope.launch {
                     val like = marketViewModel.cancelBookmark(args.nftItem.auctionId)
                     if (like == "true") {
-                        binding.ivBidNftLike.setBackgroundResource(R.drawable.ic_market_like)
+                        binding.ivNftLike.setBackgroundResource(R.drawable.ic_market_like)
                         args.nftItem.isLike = false
                     } else if (like == "false") {
-                        binding.ivBidNftLike.setBackgroundResource(R.drawable.ic_market_like_selected)
+                        binding.ivNftLike.setBackgroundResource(R.drawable.ic_market_like_selected)
                         args.nftItem.isLike = true
                     }
                 }
             }
         }
         binding.clUpload.setOnClickListener {
-        //입찰 진행
+
         }
 
     }
