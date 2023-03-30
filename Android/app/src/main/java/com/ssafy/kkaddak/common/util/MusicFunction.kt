9b53e4851b263a83943a612e6ce4a0e0dc7b0dd3
HongMinKi <gaogai2@naver.com>
2023-03-30 17:46:49 +0900

    [FEAT] 최근 거래 내역 받아오기 트랜잭션 진행

---
+++ Android/app/src/main/java/com/ssafy/kkaddak/common/util/MusicFunction.kt
@@ -0,0 +1,26 @@
+package com.ssafy.kkaddak.common.util
+
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
+import org.web3j.tx.ReadonlyTransactionManager
+import org.web3j.tx.gas.StaticGasProvider
+import java.math.BigInteger
+
+private const val INFURA_URL = "https://rpc.ssafy-blockchain.com"
+private const val NFT_CONTRACT_ADDRESS = ""
+private const val TAG = "music info"
+
+class MusicFunction {
+    // Ethereum 네트워크에 연결
+    private val web3j = Web3j.build(HttpService(INFURA_URL))
+
+    // 가스 제공자 설정
+    private val gasPrice = BigInteger.valueOf(0) // 20 Gwei
+    private val gasLimit = BigInteger.valueOf(4_300_000) // 가스 한도
+    private val contractGasProvider = StaticGasProvider(gasPrice, gasLimit)
+
+    // 트랜잭션 매니저 조회용
+    private val transactionManager = ReadonlyTransactionManager(web3j, NFT_CONTRACT_ADDRESS)
+
+
+}
