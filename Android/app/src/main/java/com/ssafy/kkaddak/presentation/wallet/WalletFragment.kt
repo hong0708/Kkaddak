9194dd554de38815c0d5c1f9d758656d69a82680
윤세림 <tpfla9953@naver.com>
2023-03-31 17:58:13 +0900

    [FEAT] Bootpay 기본 셋팅 (#S08P22D208-366)

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/wallet/WalletFragment.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/wallet/WalletFragment.kt
@@ -1,5 +1,6 @@
 package com.ssafy.kkaddak.presentation.wallet
 
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.viewModels
@@ -11,6 +12,11 @@ import com.ssafy.kkaddak.common.util.WalletFunction
 import com.ssafy.kkaddak.databinding.FragmentWalletBinding
 import com.ssafy.kkaddak.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kr.co.bootpay.android.Bootpay
+import kr.co.bootpay.android.events.BootpayEventListener
+import kr.co.bootpay.android.models.BootExtra
+import kr.co.bootpay.android.models.BootItem
+import kr.co.bootpay.android.models.Payload
 
 @AndroidEntryPoint
 class WalletFragment : BaseFragment<FragmentWalletBinding>(R.layout.fragment_wallet),
@@ -18,6 +24,7 @@ class WalletFragment : BaseFragment<FragmentWalletBinding>(R.layout.fragment_wal
 
     private val walletViewModel by viewModels<WalletViewModel>()
     private val recentTransactionListAdapter by lazy { RecentTransactionListAdapter() }
+    private var applicationId = "6426738a755e27001ead619f"
 
     override fun initView() {
         initListener()
@@ -72,9 +79,64 @@ class WalletFragment : BaseFragment<FragmentWalletBinding>(R.layout.fragment_wal
                         .show()
                 } else {
                     // 충전 플로우
+                    chargeKAT()
                 }
             }
         }
+    }
+
+    private fun chargeKAT() {
+        val extra = BootExtra()
+            .setCardQuota("0,2,3") // 일시불, 2개월, 3개월 할부 허용, 할부는 최대 12개월까지 사용됨 (5만원 이상 구매시 할부허용 범위)
+        val items: MutableList<BootItem> = ArrayList()
+        val item1 = BootItem().setName("마우's 스").setId("ITEM_CODE_MOUSE").setQty(1).setPrice(500.0)
+        val item2 = BootItem().setName("키보드").setId("ITEM_KEYBOARD_MOUSE").setQty(1).setPrice(500.0)
+        items.add(item1)
+        items.add(item2)
+        val payload = Payload()
+
+        payload.setApplicationId(applicationId)
+            .setOrderName("부트페이 결제테스트")
+            .setOrderId("1234")
+            .setPrice(1000.0)
+            .setExtra(extra).items = items
+
+        val map: MutableMap<String, Any> = HashMap()
+        map["1"] = "abcdef"
+        map["2"] = "abcdef55"
+        map["3"] = 1234
+        payload.metadata = map
+        //        payload.setMetadata(new Gson().toJson(map));
+        Bootpay.init(requireActivity().supportFragmentManager, requireActivity().applicationContext)
+            .setPayload(payload)
+            .setEventListener(object : BootpayEventListener {
+                override fun onCancel(data: String) {
+                    Log.d("bootpay", "cancel: $data")
+                }
+
+                override fun onError(data: String) {
+                    Log.d("bootpay", "error: $data")
+                }
+
+                override fun onClose() {
+                    Bootpay.removePaymentWindow()
+                }
+
+                override fun onIssued(data: String) {
+                    Log.d("bootpay", "issued: $data")
+                }
+
+                override fun onConfirm(data: String): Boolean {
+                    Log.d("bootpay", "confirm: $data")
+                    //                        Bootpay.transactionConfirm(data); //재고가 있어서 결제를 진행하려 할때 true (방법 1)
+                    return true //재고가 있어서 결제를 진행하려 할때 true (방법 2)
+                    //                        return false; //결제를 진행하지 않을때 false
+                }
+
+                override fun onDone(data: String) {
+                    Log.d("done", data)
+                }
+            }).requestPayment()
     }
 
     private fun getBalance() {
