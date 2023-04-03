49fd7692bbfed47b3e93a0b2ec30d72ff73c6f4c
Wonil Lee <lwi99556@gmail.com>
2023-04-03 23:15:37 +0900

    [FEAT] 결제 후 코인 교환 api(#S08P22D208-372)

---
+++ Server/module-api/src/main/java/com/example/kkaddak/api/service/impl/PayServiceImpl.java
@@ -0,0 +1,92 @@
+package com.example.kkaddak.api.service.impl;
+
+import com.example.kkaddak.api.dto.DataResDto;
+import com.example.kkaddak.api.dto.pay.ExchangeReqDto;
+import com.example.kkaddak.api.exception.ServiceException;
+import com.example.kkaddak.api.exception.UnauthorizationException;
+import com.example.kkaddak.api.service.KATTokenContract;
+import com.example.kkaddak.api.service.PayService;
+import com.example.kkaddak.core.entity.Member;
+import com.example.kkaddak.core.utils.ErrorMessageEnum;
+import kr.co.bootpay.Bootpay;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
+@Service
+public class PayServiceImpl implements PayService {
+    @Value("${ethereum.admin-address}")
+    private String adminAddress;
+
+    @Value("${ethereum.exchange-rate}")
+    private Integer exchageRate;
+
+    private final KATTokenContract katTokenContract;
+    private final Bootpay bootpay;
+
+    @Autowired
+    public PayServiceImpl(
+            @Qualifier("KATTokenContractWrapper") KATTokenContractWrapper katTokenContract,
+            Bootpay bootpay) {
+        this.katTokenContract = katTokenContract;
+        this.bootpay = bootpay;
+    }
+
+    @Override
+public DataResDto<?> exchangeCashToCoin(Member member, ExchangeReqDto exchangeReqDto) throws Exception {
+        // 부트 페이 접근 권한 요청
+        try {
+            HashMap token = bootpay.getAccessToken();
+            if (!Objects.isNull(token.get("error_code"))) {
+                throw new ServiceException(ErrorMessageEnum.UNAUTHORIZED_BOOTPAY.getMessage());
+            }
+        }
+        catch(Exception e){
+            throw new ServiceException(ErrorMessageEnum.UNAUTHORIZED_BOOTPAY.getMessage());
+        }
+        // 결제 정보 조회
+        HashMap result = bootpay.getReceipt(exchangeReqDto.getReceiptId());
+        if (!Objects.isNull(result.get("error_code"))) {
+            // 결제 과정에서 error 발생
+            throw new IllegalArgumentException(ErrorMessageEnum.ABNORMAL_PAYMENT.getMessage());
+        }
+        // 결제 완료 상태 이외 receipt 요청 예외 처리
+        if (!Objects.equals(result.get("status"), 1.0)) {
+            // 결제 과정에서 error 발생
+            throw new IllegalArgumentException(ErrorMessageEnum.EXPRIRED_RECEIPT.getMessage());
+        }
+        if (Objects.isNull(result.get("order_id"))){
+            // 결제 payload(account address) 누락
+            throw new IllegalArgumentException(ErrorMessageEnum.ACCOUNT_ADDRESS_REQUIRED.getMessage());
+        }
+        String accountAddress = (String) result.get("order_id");
+        if (!Objects.equals(accountAddress, member.getAccount())) {
+            // 결제자와 요청자 account address 일치 여부 확인
+            throw new UnauthorizationException(ErrorMessageEnum.UNAUTHORIZED_EXCHANGE_REQUEST.getMessage());
+        }
+        // KAT 송금 요청
+        try {
+            katTokenContract
+                    .transfer(
+                            adminAddress,
+                            accountAddress,
+                            new BigInteger(String.valueOf(Math.round((Double) result.get("price") * exchageRate))),
+                            "충전")
+                    .send();
+            return DataResDto.builder()
+                    .statusMessage("코인 구매가 완료되었습니다.")
+                    .data(true)
+                    .build();
+        }
+        catch (Exception e){
+            throw new ServiceException(e.getMessage());
+        }
+    }
+}
--- Server/module-core/src/main/java/com/example/kkaddak/core/utils/ErrorMessageEnum.java
+++ Server/module-core/src/main/java/com/example/kkaddak/core/utils/ErrorMessageEnum.java
@@ -13,7 +13,14 @@ public enum ErrorMessageEnum {
     NO_MORE_FOLLOWING("팔로잉이 존재하지 않습니다."),
     FOLLOW_NOT_EXIST("팔로우하고 있지 않는 유저입니다."),
     SONG_NOT_EXIST("음악이 존재하지 않습니다"),
-    S3_UPLOAD_ERROR("파일 업로드중 오류가 발생했습니다.");
+    S3_UPLOAD_ERROR("파일 업로드중 오류가 발생했습니다."),
+    UNAUTHORIZED_BOOTPAY("부트 페이 서버 접근 권한이 없습니다."),
+    ABNORMAL_PAYMENT("비정상적으로 결제가 이루어졌습니다."),
+    ACCOUNT_ADDRESS_REQUIRED("결제 과정에서 계정 주소가 누락되었습니다."),
+    UNAUTHORIZED_EXCHANGE_REQUEST("결제자와 요청자의 계정 주소가 다릅니다."),
+    EXPRIRED_RECEIPT("만료된 결제 정보 입니다."),
+    FOLLOW_NOT_EXIST("팔로우하고 있지 않는 유저입니다.");
+
     private String message;
 
     ErrorMessageEnum(String message) {
