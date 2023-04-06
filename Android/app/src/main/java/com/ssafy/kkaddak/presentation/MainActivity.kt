9300923772a6c63d169c6263a271df685d5a4796
HongMinKi <gaogai2@naver.com>
2023-04-06 13:44:17 +0900

    [FEAT] 생체 인증 nft 발급 및 구독에서 추가

--- Android/app/src/main/java/com/ssafy/kkaddak/presentation/MainActivity.kt
+++ Android/app/src/main/java/com/ssafy/kkaddak/presentation/MainActivity.kt
@@ -1,20 +1,9 @@
 package com.ssafy.kkaddak.presentation
 
-import android.app.Activity
-import android.app.AlertDialog
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.biometric.BiometricManager
-import androidx.biometric.BiometricPrompt
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -37,7 +26,6 @@ import com.ssafy.kkaddak.presentation.base.BaseFragment
 import com.ssafy.kkaddak.presentation.songlist.SongViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
-import java.util.concurrent.Executor
 
 
 @AndroidEntryPoint
@@ -64,9 +52,6 @@ class MainActivity : AppCompatActivity() {
         initBottomBehavior()
         initNavigation()
         initListener()
-
-        biometricPrompt = setBiometricPrompt()
-        promptInfo = setPromptInfo()
     }
 
     override fun onResume() {
@@ -287,148 +272,5 @@ class MainActivity : AppCompatActivity() {
                 binding.fabHome.visibility = View.VISIBLE
             }
         }
-    }
-
-
-    private var executor: Executor? = null
-    private var biometricPrompt: BiometricPrompt? = null
-    private var promptInfo: BiometricPrompt.PromptInfo? = null
-
-
-    private val loginLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            Log.d(TAG, "registerForActivityResult - result : $result")
-
-            if (result.resultCode == Activity.RESULT_OK) {
-                Log.d(TAG, "registerForActivityResult - RESULT_OK")
-                authenticateToEncrypt()  //생체 인증 가능 여부확인 다시 호출
-            } else {
-                Log.d(TAG, "registerForActivityResult - NOT RESULT_OK")
-            }
-        }
-
-    private fun setPromptInfo(): BiometricPrompt.PromptInfo {
-
-        val promptBuilder: BiometricPrompt.PromptInfo.Builder = BiometricPrompt.PromptInfo.Builder()
-
-        promptBuilder.setTitle("Biometric login for my app")
-        promptBuilder.setSubtitle("Log in using your biometric credential")
-        promptBuilder.setNegativeButtonText("Use account password")
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            //  안면인식 ap사용 android 11부터 지원
-            promptBuilder.setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
-        }
-
-        promptInfo = promptBuilder.build()
-        return promptInfo as BiometricPrompt.PromptInfo
-    }
-
-    private fun yourResultCallback(result: Boolean): Boolean {
-        return result
-    }
-
-    private fun setBiometricPrompt(): BiometricPrompt {
-        executor = ContextCompat.getMainExecutor(this)
-
-        biometricPrompt = BiometricPrompt(
-            this@MainActivity,
-            executor!!,
-            object : BiometricPrompt.AuthenticationCallback() {
-
-                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                    super.onAuthenticationError(errorCode, errString)
-                    Toast.makeText(
-                        this@MainActivity,
-                        """"지문 인식 ERROR [ errorCode: $errorCode, errString: $errString ]""".trimIndent(),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                    super.onAuthenticationSucceeded(result)
-                    Toast.makeText(this@MainActivity, "지문 인식 성공", Toast.LENGTH_SHORT).show()
-                    yourResultCallback(true)
-                }
-
-                override fun onAuthenticationFailed() {
-                    super.onAuthenticationFailed()
-                    Toast.makeText(this@MainActivity, "지문 인식 실패", Toast.LENGTH_SHORT).show()
-                    yourResultCallback(false)
-                }
-
-            })
-        return biometricPrompt as BiometricPrompt
-    }
-
-
-    /*
-    * 생체 인식 인증을 사용할 수 있는지 확인
-    * */
-    fun authenticateToEncrypt(): Boolean {
-
-        var allow = false
-        val biometricManager = BiometricManager.from(this@MainActivity)
-        // when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
-        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)) {
-
-            //생체 인증 가능
-            BiometricManager.BIOMETRIC_SUCCESS -> allow = true
-
-            //기기에서 생체 인증을 지원하지 않는 경우
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> allow = false
-
-            //현재 생체 인증을 사용할 수 없는 경우
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> allow = false
-
-            //생체 인식 정보가 등록되어 있지 않은 경우
-            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
-                allow = false
-
-                val dialogBuilder = AlertDialog.Builder(this@MainActivity)
-                dialogBuilder
-                    .setTitle("나의앱")
-                    .setMessage("지문 등록이 필요합니다. 지문등록 설정화면으로 이동하시겠습니까?")
-                    .setPositiveButton("확인") { dialog, which -> goBiometricSettings() }
-                    .setNegativeButton("취소") { dialog, which -> dialog.cancel() }
-                dialogBuilder.show()
-            }
-
-            //기타 실패
-            else -> allow = false
-
-        }
-
-        //인증 실행하기
-        goAuthenticate()
-
-        return allow
-    }
-
-    /*
-    * 생체 인식 인증 실행
-    * */
-    private fun goAuthenticate() {
-        Log.d(TAG, "goAuthenticate - promptInfo : $promptInfo")
-        promptInfo?.let {
-            biometricPrompt?.authenticate(it);  //인증 실행
-        }
-    }
-
-    /*
-    * 지문 등록 화면으로 이동
-    * */
-    fun goBiometricSettings() {
-        val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
-            putExtra(
-                Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-                BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL
-            )
-        }
-        loginLauncher.launch(enrollIntent)
-    }
-
-    companion object {
-        const val TAG: String = "BiometricActivity"
     }
 }
